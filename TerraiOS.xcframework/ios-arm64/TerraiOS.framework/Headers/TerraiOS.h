0eec5f26bf09382b0bd0d8cc6a37de0b174c3a77
Elliott Yu <elliottyu1@gmail.com>
2022-04-26 18:27:18 +0100

    The beginning....

---
+++ TerraiOS.xcframework/ios-arm64/TerraiOS.framework/Headers/TerraiOS.h
@@ -0,0 +1,18 @@
+//
+//  TerraiOS.h
+//  TerraiOS
+//
+//  Created by Elliott Yu on 26/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TerraiOS.
+FOUNDATION_EXPORT double TerraiOSVersionNumber;
+
+//! Project version string for TerraiOS.
+FOUNDATION_EXPORT const unsigned char TerraiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TerraiOS/PublicHeader.h>
+
+
