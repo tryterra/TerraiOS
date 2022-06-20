f3d4dc0b77da6629ba5b2d1e88982281f8160fa2
Ruben Ticehurst-James <ruben@tryterra.co>
2022-06-20 18:17:33 +0100

    [1.0.8] - added fsl pro data

--- TerraiOS.xcframework/ios-arm64/TerraiOS.framework/Headers/TerraiOS.h
+++ TerraiOS.xcframework/ios-arm64/TerraiOS.framework/Headers/TerraiOS.h
@@ -15,4 +15,3 @@ FOUNDATION_EXPORT const unsigned char TerraiOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TerraiOS/PublicHeader.h>
 
-
