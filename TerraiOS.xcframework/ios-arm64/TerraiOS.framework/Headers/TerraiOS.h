ce405dd8115b0ffc8a56c6762b86123641790faa
elliottyu19 <elliottyu1@gmail.com>
2023-11-13 14:28:51 +0000

    [1.5.3-beta] WorkoutKit Integration

--- TerraiOS.xcframework/ios-arm64/TerraiOS.framework/Headers/TerraiOS.h
+++ TerraiOS.xcframework/ios-arm64/TerraiOS.framework/Headers/TerraiOS.h
@@ -4,6 +4,7 @@
 //
 //  Created by Elliott Yu on 26/04/2022.
 //
+
 #import <Foundation/Foundation.h>
 
 //! Project version number for TerraiOS.
@@ -13,4 +14,3 @@ FOUNDATION_EXPORT double TerraiOSVersionNumber;
 FOUNDATION_EXPORT const unsigned char TerraiOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TerraiOS/PublicHeader.h>
-
